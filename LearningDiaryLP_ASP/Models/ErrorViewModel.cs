4125b12fe417bc0888d9343e5db3dd66d0bd48cc
Laparv <lastappar@gmail.com>
2022-07-20 11:32:27 +0300

    initial commit

---
+++ LearningDiaryLP_ASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LearningDiaryLP_ASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
