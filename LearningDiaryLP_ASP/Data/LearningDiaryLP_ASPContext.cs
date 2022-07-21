1f37f6f60cef07e12df7d589e3df306811529400
Laparv <lastappar@gmail.com>
2022-07-21 13:59:28 +0300

    changes to layout, connected database

---
+++ LearningDiaryLP_ASP/Data/LearningDiaryLP_ASPContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LearningDiaryLP_ASP.Models;
+
+namespace LearningDiaryLP_ASP.Data
+{
+    public class LearningDiaryLP_ASPContext : DbContext
+    {
+        public LearningDiaryLP_ASPContext (DbContextOptions<LearningDiaryLP_ASPContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LearningDiaryLP_ASP.Models.Topic> Topic { get; set; }
+    }
+}
