dad20f407fd8e329a8dc4adaf2663a6391e976e2
Laparv <lastappar@gmail.com>
2022-07-28 14:40:53 +0300

    created popup for instructions

--- LearningDiaryLP_ASP/wwwroot/js/site.js
+++ LearningDiaryLP_ASP/wwwroot/js/site.js
@@ -4,3 +4,15 @@
 // Write your JavaScript code.
 
 
+
+let popup = document.getElementById("popup");
+
+function openPopup() {
+    popup.classList.add("open-popup");
+}
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
+
+
+
